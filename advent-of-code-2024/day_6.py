35a365e6f09ed845f984202b56c2dc39a0d426e0
Koen du Buf <koen.du.buf@gmail.com>
2024-12-09 10:13:28 +0100

    Day 7 part 1

--- advent-of-code-2024/day_6.py
+++ advent-of-code-2024/day_6.py
@@ -1,7 +1,5 @@
 from aoc_general import *
 
-lines = read_input_day(6).split("\n")
-
 DIRECTIONS = [
     (0, -1),
     (1, 0),
@@ -9,30 +7,85 @@ DIRECTIONS = [
     (-1, 0),
 ]
 
-def solve_part_1():
+def read_start():
+    lines = read_input_day(6).split("\n")
     at_y = [ i for i in range(len(lines)) if any(c in lines[i] for c in "^>v<") ][0]
     at_x = [ lines[at_y].find(c) for c in "^>v<" if lines[at_y].find(c) != -1 ][0]
     at_dir = "^>v<".index(lines[at_y][at_x])
+    lines[at_y] = lines[at_y][:at_x] + "." + lines[at_y][at_x + 1:]
+    return lines, at_x, at_y, at_dir
 
+def walk(lines, at_x, at_y, at_dir):
+    next_x = at_x + DIRECTIONS[at_dir][0]
+    next_y = at_y + DIRECTIONS[at_dir][1]
+    if next_y < 0 or next_y >= len(lines) or next_x < 0 or next_x >= len(lines[next_y]):
+        return None
+    if lines[next_y][next_x] == "." :
+        return (next_x, next_y, at_dir)
+    if lines[next_y][next_x] == "#":
+        return (at_x, at_y, (at_dir + 1) % 4)
+
+def solve_part_1():
+    lines, at_x, at_y, at_dir = read_start()
     been_on_locs = set()
     been_on_locs.add((at_x, at_y))
-    lines[at_y] = lines[at_y][:at_x] + "." + lines[at_y][at_x + 1:]
-    while True:
-        next_x = at_x + DIRECTIONS[at_dir][0]
-        next_y = at_y + DIRECTIONS[at_dir][1]
-
-        if next_y < 0 or next_y >= len(lines) or next_x < 0 or next_x >= len(lines[next_y]):
-            return len(been_on_locs)
-        if lines[next_y][next_x] == "." :
-            at_x = next_x
-            at_y = next_y
-            been_on_locs.add((at_x, at_y))
-            continue
-        if lines[next_y][next_x] == "#":
-            at_dir = (at_dir + 1) % 4
-            continue
-        print(lines[next_y][next_x])
-
-submit_result_day(6, 1, solve_part_1)
-
     
+    while True:
+        w = walk(lines, at_x, at_y, at_dir)
+        if w is None:
+            return len(been_on_locs)
+        at_x, at_y, at_dir = w
+        been_on_locs.add((at_x, at_y))
+
+def solve_part_2():
+    lines, start_x, start_y, start_dir = read_start()
+    at_x, at_y, at_dir = start_x, start_y, start_dir
+
+    def test_looping(lines, from_x, from_y, from_dir):
+        # See if we end up in a loop
+        been_in_state = set()
+        while True:
+            been_in_state.add((from_x, from_y, from_dir))
+            w = walk(lines, from_x, from_y, from_dir)
+            if w is None:
+                return False
+            from_x, from_y, from_dir = w
+            if (from_x, from_y, from_dir) in been_in_state:
+                return True
+        raise Exception("Should not reach here")
+
+    def would_be_in_a_loop(lines, at_x, at_y, at_dir):
+        # Check if we can put a wall there
+        w = walk(lines, at_x, at_y, at_dir)
+        if w is None:
+            return False
+        next_x, next_y, _ = w
+        now_is = lines[next_y][next_x]
+        if now_is == "#":
+            return False
+        # If so, put a wall there, and check if we end up in a loop
+        lines[next_y] = lines[next_y][:next_x] + "#" + lines[next_y][next_x + 1:]
+        looping = test_looping(lines, at_x, at_y, at_dir)
+        lines[next_y] = lines[next_y][:next_x] + now_is + lines[next_y][next_x + 1:]
+        return (next_x, next_y) if looping else False
+
+    obstruction_locs = set()
+    while True:
+        # Check if we would end up in a loop if we put a wall in front of us
+        obs = would_be_in_a_loop(lines, at_x, at_y, at_dir)
+        if obs != False:
+            obstruction_locs.add(obs)
+        # Then walk as usual
+        w = walk(lines, at_x, at_y, at_dir)
+        if w is None:
+            if (start_x, start_y) in obstruction_locs:
+                obstruction_locs.remove((start_x, start_y))
+            return len(obstruction_locs)
+        at_x, at_y, at_dir = w
+        
+
+# submit_result_day(6, 1, solve_part_1)
+
+# submit_result_day(6, 2, solve_part_2)
+
+print(solve_part_2())
--- advent-of-code-2024/day_7.py
+++ advent-of-code-2024/day_7.py
@@ -1,3 +1,47 @@
 from aoc_general import *
 
-data = read_input_day(7)
+SIGMA = 0.00000000001
+
+lines = read_input_day(7).split("\n")
+combos = [ ( int(line.split(": ")[0]), [ int(n) for n in line.split(": ")[1].split(" ") ]) for line in lines ]
+
+def is_possible_1(result, with_nums):
+    if len(with_nums) == 1:
+        return abs(with_nums[0] - result) < SIGMA
+    return is_possible_1(result - with_nums[-1], with_nums[:-1]) or is_possible_1(result / with_nums[-1], with_nums[:-1])
+
+def solve_part_1():
+    total_possible = 0
+    for res, nums in combos:
+        if is_possible_1(res, nums):
+            total_possible += res
+    return total_possible
+
+def is_possible_2(result, with_nums):
+    if len(with_nums) == 1:
+        return abs(with_nums[0] - result) < SIGMA
+    
+    if abs(int(result) - result) < SIGMA and str(result).endswith(str(with_nums[-1])):
+        new_res = str(result)[:-len(str(with_nums[-1]))]
+        if len(new_res) == 0:
+            if is_possible_2(1, with_nums[:-1]) or is_possible_2(0, with_nums[:-1]):
+                return True
+        elif is_possible_2(int(new_res), with_nums[:-1]):
+            return True
+        
+    return is_possible_2(result - with_nums[-1], with_nums[:-1]) or is_possible_2(result / with_nums[-1], with_nums[:-1])
+
+def solve_part_2():
+    total_possible = 0
+    for res, nums in combos:
+        if is_possible_2(res, nums):
+            total_possible += res
+    return total_possible
+
+
+print(solve_part_2())
+
+# submit_result_day(7, 1, solve_part_1)
+
+# submit_result_day(7, 2, solve_part_2())
+    
