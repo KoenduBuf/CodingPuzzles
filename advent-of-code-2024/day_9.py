5c9350bde51d0bf954ca8c00bdaf4a581aa67282
Koen du Buf <koendb@delta-n.nl>
2024-12-11 17:28:43 +0100

    Day 9 part 2

--- advent-of-code-2024/day_9.py
+++ advent-of-code-2024/day_9.py
@@ -58,12 +58,22 @@ def move_compress_by_whole_file(alternating):
             at_pos -= 2
             continue
         to = find_spot(size)
+        # display_alternating_with_id(alternating_with_id)
+        # print(f"Moving {id} with size {size} from pos {at_pos} to {to} (len {len(alternating_with_id)})")
         if to == -1 or to >= at_pos:
             at_pos -= 2
             at_f_id -= 1
             continue
         spot_size = alternating_with_id[to][1]
         
+        if to + 1 == at_pos:
+            if to + 2 < len(alternating_with_id):
+                alternating_with_id[to+2] = (-1, alternating_with_id[to][1] + alternating_with_id[to+2][1])
+            alternating_with_id[to] = (-1, 0)
+            at_pos -= 2
+            at_f_id -= 1
+            continue
+
         size_free_at_f_pos = size
         size_free_at_f_pos += alternating_with_id[at_pos-1][1]
         if at_pos < len(alternating_with_id) - 1:
@@ -72,12 +82,18 @@ def move_compress_by_whole_file(alternating):
         alternating_with_id[to+3:at_pos+1] = alternating_with_id[to+1:at_pos-1]
         alternating_with_id[to] = (-1, 0)
         alternating_with_id[to+1] = (id, size)
-        alternating_with_id[to+2] = (-1, spot_size - size)
+
+        if to < len(alternating_with_id) - 2:
+            alternating_with_id[to+2] = (-1, spot_size - size)
 
         if at_pos < len(alternating_with_id) - 1:
             alternating_with_id[at_pos+1] = (-1, size_free_at_f_pos)
         at_f_id -= 1
-        
+
+    # 6311837662089 <- correct
+    # 6311872499717
+
+    # display_alternating_with_id(alternating_with_id)
     
     # total_disk_size_1 = sum([ n for n in alternating ])
     # total_disk_size_2 = sum([ n for i, n in alternating_with_id ])
@@ -103,6 +119,7 @@ def move_compress_by_whole_file(alternating):
         for _ in range(alternating_with_id[i][1]):
             if alternating_with_id[i][0] == -1:
                 raise Exception("Should not have any -1s")
+            # print(f"Adding {at_pos} * {alternating_with_id[i][0]}")
             total_checksum += at_pos * alternating_with_id[i][0]
             at_pos += 1
 
@@ -120,12 +137,7 @@ def solve_part_2():
     return move_compress_by_whole_file([ e for e in disk_nums_alternating ])
 
 
-# print(solve_part_2())
 
-# submit_result_day(9, 1, solve_part_1)
+submit_result_day(9, 1, solve_part_1)
 
 submit_result_day(9, 2, solve_part_2)
-
-
-
-
